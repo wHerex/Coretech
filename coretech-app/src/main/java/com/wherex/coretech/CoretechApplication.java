e8a0c651a2bddc8bc567228e65f1391a59ec4758
Marcin Ogrodniczak <mogrodniczak@nsa.gov.pl>
2021-02-26 17:46:42 +0100

    Initial commit

---
+++ coretech-app/src/main/java/com/wherex/coretech/CoretechApplication.java
@@ -0,0 +1,13 @@
+package com.wherex.coretech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoretechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoretechApplication.class, args);
+	}
+
+}
---
+++ coretech-app/src/test/java/com/wherex/coretech/CoretechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wherex.coretech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoretechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
