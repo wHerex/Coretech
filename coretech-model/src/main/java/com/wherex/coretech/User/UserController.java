423c91f55197c4bba69d180312b22bc17f954cfe
Marcin Ogrodniczak <mogrodniczak@nsa.gov.pl>
2021-05-14 17:41:24 +0200

    WIP

--- coretech-model/src/main/java/com/wherex/coretech/User/UserController.java
+++ coretech-model/src/main/java/com/wherex/coretech/User/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/product")
+@RequestMapping(path = "/users")
 @CrossOrigin
 public class UserController {
 
