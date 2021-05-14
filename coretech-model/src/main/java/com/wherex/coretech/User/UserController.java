ff93a1624d7a06fa1899f78cb583286b0a180db8
Marcin Ogrodniczak <mogrodniczak@nsa.gov.pl>
2021-05-14 19:35:32 +0200

    WIP

--- coretech-model/src/main/java/com/wherex/coretech/User/UserController.java
+++ coretech-model/src/main/java/com/wherex/coretech/User/UserController.java
@@ -5,6 +5,7 @@ import com.wherex.coretech.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,16 +17,27 @@ public class UserController {
 
     @Autowired
     public UserController() {
-        users = List.of(
-                new User("admin", "admin", "ADMIN"),
-                new User("user", "user", "USER")
-        );
+        users = new ArrayList<>();
+        users.add(new User("admin", "admin", "ADMIN"));
+        users.add(new User("user", "user", "USER"));
+
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public Callback login(@RequestBody User user) {
         if (users.contains(user))
             return new Callback(user, Status.SUCCESS);
         return new Callback(user, Status.FAILURE);
+    }
+
+    @PostMapping("/register")
+    public Callback register(@RequestBody User user){
+        if (!users.contains(user)){
+            users.add(user);
+            return new Callback(user, Status.SUCCESS);
+        }else{
+            return new Callback(user, Status.FAILURE);
+        }
+
     }
 }
